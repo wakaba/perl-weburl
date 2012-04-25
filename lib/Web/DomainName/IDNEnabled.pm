5dadb4aecef7884ff1c6b05f68c5d4bdcddd6892
Wakaba <w@suika.fam.cx>
2012-04-25 15:18:40 +0900

    make dataautoupdate

--- lib/Web/DomainName/IDNEnabled.pm
+++ lib/Web/DomainName/IDNEnabled.pm
@@ -1,6 +1,6 @@
 package Web::DomainName::IDNEnabled;
-our $VERSION = '1.0.1334848620';
-$TIMESTAMP = 1334848620;
+our $VERSION = '1.0.1335334720';
+$TIMESTAMP = 1335334720;
 
 ## This module is automatically generated.  Don't edit!
 
@@ -99,7 +99,7 @@ Web::DomainName::IDNEnabled - List of IDN-enabled TLDs
 
 The C<Web::DomainName::IDNEnabled> module provides a list of IDN-enabled
 TLDs.  It contains TLDs listed in the I<IDN-enabled TLDs> (as of
-2012-04-24) maintained by the Mozilla project.
+2012-04-25) maintained by the Mozilla project.
 
 =head1 VARIABLE
 
